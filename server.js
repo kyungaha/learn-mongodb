c4c9528ca7c257c6533f1ce5a32b8598c78f39a8
kyungaha <buzzka91@naver.com>
2025-07-15 11:40:49 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+//require("express")
+import express from "express";
+
+const app = express();
+
+app.listen(8080, (req, res) => {
+    console.log("server running at..");
+    console.log("추가!!!");
+    console.log("추가222");
+});
